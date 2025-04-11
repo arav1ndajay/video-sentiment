25ce0b569ca46e24af9068fe9ad23e9de535bed6
arav1ndajay <aravindajay11@gmail.com>
2025-04-11 21:27:16 +0530

    Changing versions

--- lib/transcript-api.ts
+++ lib/transcript-api.ts
@@ -11,7 +11,7 @@ export async function getVideoTranscript(videoId: string) {
       args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath(
-        `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+        `https://github.com/Sparticuz/chromium/releases/download/v133.0.0/chromium-v133.0.0-pack.tar`
       ),
       headless: true,
     });
